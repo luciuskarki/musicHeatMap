e878d68eb4433b53252baa4eb0e4724b82653b81
Solarific <roflwotnoob@live.com>
2024-04-03 20:53:20 -0400

    starting basis for profile ui

--- Front/MusicHeatMapReact/src/screens/ProfilePage.tsx
+++ Front/MusicHeatMapReact/src/screens/ProfilePage.tsx
@@ -17,10 +17,58 @@ import {
 
   function ProfileScreen() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Customize Your Profile!</Text>
-      </View>
+      <SafeAreaView style = {styles.container}>
+        <View style = {styles.profileBorder}>
+          <View style = {styles.profileImageContainer}> 
+          </View>
+        </View>
+      </SafeAreaView>
     );
   }
   
   export default ProfileScreen;
+
+              
+var textColor = "#52b788" //b864b9 pink 95d5b2 green
+var bgColor = "#1b2021"
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor:bgColor ,
+  
+    },  
+    profileBorder:{
+        flexDirection: 'row',
+        borderColor: textColor,
+        borderWidth: 2 ,
+        borderRadius: 1,
+        padding: 10,
+        backgroundColor: bgColor,
+        paddingHorizontal: 20,
+        paddingVertical: 100,
+    },
+    HistoryText: {
+        fontSize: 20,
+        color: textColor
+      },
+    
+    buttonContainer:{
+        flex:3
+    },
+    ImagesContainer:{
+        alignContent: 'center',
+        flexDirection: 'row',
+        flex:1,
+        
+    },
+    profileImageContainer:{
+      borderColor: textColor,
+      borderWidth: 5 ,
+      borderRadius: 100,
+      backgroundColor: bgColor,
+      paddingHorizontal: 40,
+      paddingVertical: 40,
+      marginRight: 40,
+    }
+    });
