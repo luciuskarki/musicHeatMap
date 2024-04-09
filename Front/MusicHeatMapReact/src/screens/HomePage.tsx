624cead397b46fe1076b73bd9a9d6affe392c955
lkarki <lucius.karki@mymail.champlain.edu>
2024-04-09 15:36:02 -0400

    Not working code for the search button. Figuring out Modal/pressables

--- Front/MusicHeatMapReact/src/screens/HomePage.tsx
+++ Front/MusicHeatMapReact/src/screens/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 
 import {
     SafeAreaView,
@@ -13,20 +13,44 @@ import {
     Button,
     Alert,
     TouchableOpacity,
-    Platform
+    Platform,
+    Pressable,
+    Modal
   } from 'react-native';
 
 import MapView, {Heatmap, Marker, PROVIDER_GOOGLE} from 'react-native-maps'
+
+var textColor = "#52b788" //b864b9 pink 95d5b2 green
+var bgColor = "#1b2021"
+const [modalVisible, setModalVisible] = useState(false);
 
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
        justifyContent: 'flex-end',
        alignItems: 'center',
+       height: "90%"
   },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
+  postButton:{
+    borderRadius: 50,
+    margin: 20,
+    alignContent: 'center',
+    height: "10%",
+    width: "90%",
+    marginBottom: 20,
+    paddingVertical: 20,
+    paddingHorizontal: 12
+    },
+  postButtonText:{
+    fontSize: 30,
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase",
+    alignContent: 'center'
+  }
 });
 
 function HomePage() {
@@ -42,22 +66,53 @@ let points=[{latitude: 44.473215997359425, longitude: -73.20414593214373, weight
         <View style={styles.container}>
             <MapView
                 //this is where you can change the properties of the map
-                                                     provider={PROVIDER_GOOGLE}
-                                                     style={styles.map}
-                                                     initialRegion={{
-                                                        latitude: 44.473215997359425,
-                                                        longitude: -73.20414593214373,
-                                                        latitudeDelta: 0.0922,
-                                                        longitudeDelta: 0.0421,
-                                                        }}
-                                                  >
+                provider={PROVIDER_GOOGLE}
+                style={styles.map}
+                initialRegion={{
+                    latitude: 44.473215997359425,
+                    longitude: -73.20414593214373,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                    }}
+              >
                 <Heatmap
                     //we can potentially change the gradient of the heatmap
                     points={points}
 
                             >
                 </Heatmap>
+
             </MapView>
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={modalVisible}
+              onRequestClose={() => {
+                Alert.alert('Modal has been closed.');
+                setModalVisible(!modalVisible);
+              }}>
+              <View>
+                <View>
+                  <Text>Hello World!</Text>
+                  <Pressable
+                    onPress={() => setModalVisible(!modalVisible)}>
+                    <Text>Hide Modal</Text>
+                  </Pressable>
+                </View>
+              </View>
+            </Modal>
+            <Pressable
+              onPress={()=>setModalVisible(true)}
+              style={({pressed}) => [
+                {
+                  backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
+                },
+                styles.postButton,
+              ]}>
+              {({pressed}) => (
+                <Text style={styles.postButtonText}>{pressed ? 'Posting your song!' : 'Post your song!'}</Text>
+              )}
+              </Pressable>
         </View>
     ); 
 }
