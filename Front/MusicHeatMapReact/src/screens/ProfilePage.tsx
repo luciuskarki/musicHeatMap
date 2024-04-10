3afabb61bc56c52df9d7c98d3c9593ed2890c752
Solarific <roflwotnoob@live.com>
2024-04-09 20:46:38 -0400

    Added stack navigation within profile settings, for onPress Buttons

--- Front/MusicHeatMapReact/src/screens/ProfilePage.tsx
+++ Front/MusicHeatMapReact/src/screens/ProfilePage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import {
     SafeAreaView,
     ScrollView,
@@ -15,22 +16,61 @@ import {
     TouchableOpacity
   } from 'react-native';
 
-  function ProfileScreen() {
-    return (
-      <SafeAreaView style = {styles.container}>
-        <View style = {styles.profileBorder}>
-          <View style = {styles.profileImageContainer}> 
-                      <Image source={require("../assets/important_photo_test.jpg")}
-                           style = {styles.ImagesContainer}>
-                      </Image>
-          </View>
-          <View style = {styles.userTextContainer}>
-          <Text style = {styles.userText}>{`Username \nUniqueID `}</Text>
-          </View>
+
+
+  const HomeProfile = props => {
+    const onPress = () => {
+      props.navigation.navigate('ScreenTwo');
+    };
+    
+  return (
+    <SafeAreaView style = {styles.container}>
+      <View style = {styles.profileBorder}>
+        <View style = {styles.profileImageContainer}> 
+           <Image source={require("../assets/important_photo_test.jpg")}
+                  style = {styles.ImagesContainer}>
+            </Image>
         </View>
-      </SafeAreaView>
+        <View style = {styles.userTextContainer}>
+          <Text style = {styles.userText}>{`Username \nUniqueID `}</Text>
+        </View> 
+      </View>
+      
+    <View style = {styles.buttonContainer}>
+      <TouchableOpacity onPress={onPress}>
+        <Text style = {styles.buttonText}> Profile Settings</Text>
+      </TouchableOpacity>
+    </View>
+
+
+
+
+      
+    </SafeAreaView>
+  );
+  } //add buttons between 2 views and then apply to "ScreenTwo"
+
+  const ProfileSettings = () => {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>PlaceHolder</Text>
+      </View>
     );
-  }
+  };
+
+
+  function ProfileScreen() {
+    
+    const Stack = createStackNavigator();
+
+    return (
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="ScreenOne" component={HomeProfile} />
+          <Stack.Screen name="ScreenTwo" component={ProfileSettings} />
+        </Stack.Navigator>
+    );
+  };
+
   
   export default ProfileScreen;
 
@@ -47,7 +87,6 @@ var bgColor = "#1b2021"
     profileBorder:{
         flexDirection: 'row',
         borderColor: textColor,
-        borderBottomColor: "red",
         borderBottomWidth: 2,
         borderRadius: 1,
         padding: 0,
@@ -57,7 +96,15 @@ var bgColor = "#1b2021"
     },
     
     buttonContainer:{
-        flex:3
+      borderColor: textColor,
+      borderBottomWidth: 2,
+      borderRadius: 1,
+      padding: 0,
+      backgroundColor: bgColor,
+    },
+    buttonText:{
+      fontSize: 33,
+      color:textColor
     },
     ImagesContainer:{
       resizeMode: 'cover',
