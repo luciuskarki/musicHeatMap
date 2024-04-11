b3059e3409391509c1dcf37c4fa1fcd26a58eb05
nicoleamaral37 <nicole.amaral@mymail.champlain.edu>
2024-04-11 11:32:36 -0400

    oh god

--- Front/MusicHeatMapReact/src/screens/HomePage.tsx
+++ Front/MusicHeatMapReact/src/screens/HomePage.tsx
@@ -1,4 +1,6 @@
+
 import React, {Component} from 'react';
+import Geolocation from "react-native-geolocation-service";
 
 import {
     SafeAreaView,
@@ -13,10 +15,11 @@ import {
     Button,
     Alert,
     TouchableOpacity,
-    Platform
+    Platform,
+    PermissionsAndroid
   } from 'react-native';
 
-import MapView, {Heatmap, Marker, PROVIDER_GOOGLE} from 'react-native-maps'
+import MapView, {Polyline, Heatmap, Marker, PROVIDER_GOOGLE} from 'react-native-maps'
 
 const styles = StyleSheet.create({
   container: {
@@ -29,36 +32,94 @@ const styles = StyleSheet.create({
   },
 });
 
-function HomePage() {
+export default class Map extends React.Component<any,any>{
+  
+    constructor(props) {
+      super(props);
+       this.state = {
+          latitude: 0,
+          longitude: 0,
+          coordinates: [],
+       };
+     }
+      requestLocationPermission = async () => {
+      try {
+        let granted = await PermissionsAndroid.request(
+          
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: 'Geolocation Permission',
+            message: 'Can we access your location?',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
+          },
+        );
+        console.log('granted', granted);
+        if (granted === 'granted') {
+          console.log('You can use Geolocation');
+          return true;
+        } else {
+          console.log('You cannot use Geolocation');
+          return false;
+        }
+      } catch (err) {
+        return false;
+      }
+    };
 
+     async componentDidMount() {
+      let result = this.requestLocationPermission();
+      result.then(res => {
+        console.log('res is:', res);
+        if (res) {
+      Geolocation.getCurrentPosition(
+        position => {
+          this.setState({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            coordinates: this.state.coordinates.concat({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            }),
+          });
+          console.log("location" + this.state.latitude)
+        },
+        error => {
+          Alert.alert(error.message.toString());
+        },
+        {
+          enableHighAccuracy: true, timeout: 15000, maximumAge: 10000
+        },
+      );
+    }
+    });
+  }
 //if this is where we can pass in the data to heatmap
-let points=[{latitude: 44.473215997359425, longitude: -73.20414593214373, weight: 1},
-            {latitude: 44.474215997359425, longitude: -73.20314593214373, weight: 1},
-            {latitude: 44.474215997359425, longitude: -73.20214593214373, weight: 1},
-            {latitude: 44.473215997359425, longitude: -73.20314593214373, weight: 1}
-            ]
-
+    render() {    
     return (
+      
         <View style={styles.container}>
             <MapView
                 //this is where you can change the properties of the map
-                                                     provider={PROVIDER_GOOGLE}
-                                                     style={styles.map}
-                                                     initialRegion={{
-                                                        latitude: 44.473215997359425,
-                                                        longitude: -73.20414593214373,
-                                                        latitudeDelta: 0.0922,
-                                                        longitudeDelta: 0.0421,
-                                                        }}
-                                                  >
-                <Heatmap
-                    //we can potentially change the gradient of the heatmap
-                    points={points}
-
-                            >
-                </Heatmap>
+               provider={PROVIDER_GOOGLE}
+               style={styles.map}
+               region={{
+              latitude: this.state.latitude,
+               longitude: this.state.longitude,
+               latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+                }}>
+              <Marker
+              coordinate={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+                }}>
+              </Marker>
             </MapView>
         </View>
+        
     ); 
+  }
 }
-export default HomePage;
+
